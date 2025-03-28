b57f6a6aa8e05f5a75fb08a3140ad86a21cc9174
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-03-28 11:29:36 +0500

    Fixing DB connection errors

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -9,7 +10,8 @@ import (
 var DB *sql.DB
 
 func InitDB() {
-	DB, err := sql.Open("sqlite3", "api.db")
+	var err error
+	DB, err = sql.Open("sqlite3", "api.db")
 
 	if err != nil {
 		panic("Could not connect to database.")
@@ -24,17 +26,38 @@ func InitDB() {
 func createTables() {
 	createEventsTable := `
 	CREATE TABLE IF NOT EXISTS events (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	name TEXT NOT NULL,
-	description TEXT NOT NULL,
-	location TEXT NOT NULL,
-	dateTime DATETIME NOT NULL,
-	user_id INTEGER
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		description TEXT NOT NULL,
+		location TEXT NOT NULL,
+		dateTime DATETIME NOT NULL,
+		user_id INTEGER
 	)
 	`
 
 	_, err := DB.Exec(createEventsTable)
+
 	if err != nil {
+		fmt.Println(err)
 		panic("Could not create events table.")
 	}
+
 }
+
+// func createTables() {
+// 	createEventsTable := `
+// 	CREATE TABLE IF NOT EXISTS events (
+// 	id INTEGER PRIMARY KEY AUTOINCREMENT,
+// 	name TEXT NOT NULL,
+// 	description TEXT NOT NULL,
+// 	location TEXT NOT NULL,
+// 	dateTime DATETIME NOT NULL,
+// 	user_id INTEGER
+// 	)
+// 	`
+
+// 	_, err := DB.Exec(createEventsTable)
+// 	if err != nil {
+// 		panic("Could not create events table.")
+// 	}
+// }
