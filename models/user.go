3e13abcc77f8b12388b1dd1299be6a88a88232d8
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-04-01 18:28:32 +0500

    Login Route

--- models/user.go
+++ models/user.go
@@ -37,3 +37,15 @@ func (u User) Save() error {
 	u.ID = userId
 	return err
 }
+
+func (u User) ValidateCredentials() error {
+	query := "SELECT password FROM users WHERE email = ?"
+	row := db.DB.QueryRow(query, u.Email)
+
+	var retrievedPassword string
+	err := row.Scan(&retrievedPassword)
+
+	if err != nil {
+		return err
+	}
+}
